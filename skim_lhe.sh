3a9af89d0061319207abce672a526b60de4aa731
lucien1011 <lucien1011@gmail.com>
2019-05-20 08:53:34 -0400

    First commit

---
+++ skim_lhe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#root -l -q ZZD_lhe.C\(\"/home/rosedj1/DarkZ-EvtGeneration/CMSSW_9_4_2/src/DarkZ-EvtGeneration/workDir_ppTOzzp_nohh2_eps5e-2_mZd20/UnpackTarball/cmsgrid_final_ppTOzzp_nohh2.lhe\",10000\)
+root -l -q ZZD_lhe.C\(\"cmsgrid_final_ppTOzzp_nohh2.lhe\",10000\)
